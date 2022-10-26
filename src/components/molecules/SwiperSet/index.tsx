6a25298b0c36afbc56fc26b9037fa76441ca33c7
TENO <2649821154@qq.com>
2022-10-26 18:56:47 +0800

    feat: finish append swiper item

--- src/components/molecules/SwiperSet/index.tsx
+++ src/components/molecules/SwiperSet/index.tsx
@@ -5,30 +5,23 @@ import { Dispatch } from '@reduxjs/toolkit'
 import { CheckBox } from '@/components/atoms/CheckBox'
 import { useGetValue } from '@/hooks/useGetValue'
 import { useSelector } from 'react-redux'
-import { selectSwiper } from '@/store/swiper.slice'
+import { selectSwiper, swiperSliceAction } from '@/store/swiper.slice'
 import { useSetCheckBox } from '@/hooks/useSetCheckBox'
 import { StyleInput } from '@/components/atoms/StyleInput'
 import { SwiperItem } from '@/components/atoms/SwiperItem'
 import { useUpdate } from '@/hooks/useUpdate'
+import { useDispatch } from 'react-redux'
 interface Props {
     target: HTMLElement,
     dispatch: Dispatch
 }
 export const SwiperSet = (props: Props) => {
+    const dispatch = useDispatch()
     const container = useRef<any>()
     const arrowRef = useRef<any>()
     const whole = useRef<any>()
-    useLayoutEffect(() => {
-        whole.current.style.display = 'none'
-        if (props.target !== null) {
-            if (props.target.id === 'swiper') {
-                whole.current.style.display = 'flex'
-            }
-        }
-    }, [props])
-
     const swiper = useSelector(selectSwiper)
-    const [update, pivot] = useUpdate()
+    const [update] = useUpdate()
     const [autoplay, setAutoPlay] = useSetCheckBox('autoPlay', props.dispatch)
     const [pagination, setPagination] = useSetCheckBox('pagination', props.dispatch)
     const [scrollbar, setScrollbar] = useSetCheckBox('scrollbar', props.dispatch)
@@ -37,12 +30,30 @@ export const SwiperSet = (props: Props) => {
     const ensure = useRef<any>()
     const purgeContainer = usePurge(container.current, arrowRef.current, 76 + 48 + (108 * swiper.items.length))
     useLayoutEffect(() => {
-        if (swiper.garbage !== 0) {
+        whole.current.style.display = 'none'
+        if (props.target !== null) {
+            if (props.target.id === 'swiper') {
+                whole.current.style.display = 'flex'
+            }
+        }
+    }, [props])
+    const deleteItem = () => {
+        update()
+        ensure.current.style.opacity = 0
+        setTimeout(() => {
+            setShow(false)
+        }, 100)
+    }
+    const appendItem = () => {
+        dispatch(swiperSliceAction.appendContent())
+        update()
+    }
+    useLayoutEffect(() => {
+        if (swiper.garbage > 0) {
             setShow(true)
             setTimeout(() => {
                 ensure.current.style.opacity = 1
             })
-
         }
     }, [swiper.garbage])
     return (
@@ -52,10 +63,16 @@ export const SwiperSet = (props: Props) => {
                 <img src={arrow} ref={arrowRef} alt="" />
             </div>
             <div className="attribute-content" ref={container}>
+
                 <CheckBox title='自动播放' labelId='autoplay' value={autoplay} setValue={setAutoPlay} />
                 <CheckBox title='轮播点' labelId='pagination' value={pagination} setValue={setPagination} />
                 <StyleInput tip='播放间隔' title='DL' value={delay} changeValue={setDelay} hoc={true} />
                 <CheckBox title='滚动条' labelId='scrollbar' value={scrollbar} setValue={setScrollbar} />
+                <div className="attribute-content-create">
+                    <div className="attribute-content-create-btn" onMouseUp={appendItem}>
+                        添加图片
+                    </div>
+                </div>
                 {swiper.items.map(item =>
                     item.status &&
                     <SwiperItem
@@ -65,7 +82,7 @@ export const SwiperSet = (props: Props) => {
                 <div className="attribute-content-sure">
                     {
                         show &&
-                        <div className="attribute-content-sure-btn" ref={ensure} onMouseUp={update}>
+                        <div className="attribute-content-sure-btn" ref={ensure} onMouseUp={deleteItem}>
                             确认删除
                         </div>
                     }
